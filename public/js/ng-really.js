5956af3a121fbb0e412184ce26c494cb657a9e37
vincent <spittt10@gmail.com>
2014-03-16 13:31:01 +0100

    ajout popup validation suppression. Création directive

---
+++ public/js/ng-really.js
@@ -0,0 +1,21 @@
+/**
+ * Created by administrateur on 16/03/14.
+ */
+
+/**
+ * A generic confirmation for risky actions.
+ * Usage: Add attributes: ng-really-message="Are you sure"? ng-really-click="takeAction()" function
+ */
+angular.module('mean').directive('ngReallyClick', [function() {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            element.bind('click', function() {
+                var message = attrs.ngReallyMessage;
+                if (message && confirm(message)) {
+                    scope.$apply(attrs.ngReallyClick);
+                }
+            });
+        }
+    }
+}]);
